0a0a1769573110bdc17d49ba0332e7fa3fa1589e
dendrobiummoniliforme <84551272+dendrobiummoniliforme@users.noreply.github.com>
2025-01-18 10:59:48 -0500

    Refactor map; refactor player to include map refactor.

--- src/map.rs
+++ src/map.rs
@@ -12,20 +12,127 @@ pub enum TileType {
     Floor,
 }
 
-/// Get the idx for an xy coordinate.
-pub fn xy_idx(x: i32, y: i32) -> usize {
-    // We use usize such that we do not
-    // return a negative idx.
-    // As usize is unsigned.
-    (y as usize * 80) + (x as usize)
-}  
+pub struct Map {
+    pub tiles: Vec<TileType>,
+    pub rooms: Vec<Rect>,
+    pub width: i32,
+    pub height: i32
+}
 
-/// Given a Room and a slice of Map apply the Room to the Map by
-/// mutating it.
-pub fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
-    for y in room.y1 + 1 ..= room.y2 {
-        for x in room.x1 ..= room.x2 {
-            map[xy_idx(x, y)] = TileType::Floor;
+impl Map {
+    /// Get the idx for an xy coordinate.
+    pub fn xy_idx(&self, x: i32, y: i32) -> usize {
+        // We use usize such that we do not
+        // return a negative idx.
+        // As usize is unsigned.
+        (y as usize * 80) + (x as usize)
+    }  
+
+    /// Given a Room and a slice of Map apply the Room to the Map by
+    /// mutating it.
+    pub fn apply_room_to_map(&mut self, room: &Rect, map: &mut [TileType]) {
+        for y in room.y1 + 1 ..= room.y2 {
+            for x in room.x1 ..= room.x2 {
+                map[self.xy_idx(x, y)] = TileType::Floor;
+            }
+        }
+    }
+
+    /// Makes a map with solid boundaries and 400 randomly placed walls. No guarentees that
+    /// it won't look awful.
+    pub fn new_map_test(&mut self) -> Vec<TileType> {
+        let mut map = vec![TileType::Floor; 80*50];
+        
+        for x in 0..80 {
+            map[self.xy_idx(x, 0)] = TileType::Wall;
+            map[self.xy_idx(x, 49)] = TileType::Wall;
+        }
+
+        for y in 0..50 {
+            map[self.xy_idx(0, y)] = TileType::Wall;
+            map[self.xy_idx(79, y)] = TileType::Wall;
+        }
+
+        // Now we will randomly splat a bunch of walls. It won't be pretty, 
+        // but it's a decent illustration.
+        // First, obtain te thread-local RNG:
+        let mut rng = rltk::RandomNumberGenerator::new();
+
+        for _i in 0..400 {
+            let x = rng.roll_dice(1, 79);
+            let y = rng.roll_dice(1, 49);
+            let idx = self.xy_idx(x, y);
+
+            if idx != self.xy_idx(40, 25) {
+                map[idx] = TileType::Wall;
+            }
+        }
+
+        map
+    }
+
+    /// Makes a map with two rooms connected by corridors.
+    /// Returns a list of rooms.
+    pub fn new_map_rooms_and_corridors(&mut self) -> (Vec<Rect>, Vec<TileType>) {
+        let mut map = vec![TileType::Wall; 80*50];
+        
+        let mut rooms: Vec<Rect> = Vec::new();
+        const MAX_ROOMS: i32 = 30;
+        const MIN_SIZE: i32 = 6;
+        const MAX_SIZE: i32 = 10;
+
+        let mut rng = RandomNumberGenerator::new();
+
+        for _ in 0..MAX_ROOMS {
+            let w = rng.range(MIN_SIZE, MAX_SIZE);
+            let h = rng.range(MIN_SIZE, MAX_SIZE);
+            let x = rng.roll_dice(1, 80 - w - 1) - 1;
+            let y = rng.roll_dice(1, 50 - h - 1) - 1;
+            let new_room = Rect::new(x, y, w, h);
+            let mut ok = true;
+
+            for other_room in rooms.iter() {
+                if new_room.intersect(other_room) {
+                    ok = false;
+                }
+            }
+            if ok {
+                self.apply_room_to_map(&new_room, &mut map);
+
+                if !rooms.is_empty() {
+                    let (new_x, new_y) = new_room.center();
+                    let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                    if rng.range(0, 2) == 1 {
+                        self.apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
+                        self.apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+                    } else {
+                        self.apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
+                        self.apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+                    }
+                }
+                rooms.push(new_room);
+            }
+        }
+
+        (rooms, map)
+    }  
+
+    fn apply_horizontal_tunnel(&mut self, map: &mut [TileType], x1: i32, x2: i32, y: i32) {
+        for x in min(x1, x2) ..= max(x1, x2) {
+            let idx = self.xy_idx(x, y);
+            if idx > 0 && idx < 80*50 {
+                map[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_vertical_tunnel(&mut self, map: &mut [TileType], y1: i32, y2: i32, x: i32) {
+        for y in min(y1, y2) ..= max(y1, y2) {
+            let idx = self.xy_idx(x, y);
+            if idx > 0 && idx < 80*50 {
+                map[idx as usize] = TileType::Floor;
+            }
         }
     }
 }
@@ -33,7 +140,7 @@ pub fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
 /// Given a slice of Map, and the ctx
 /// Apply a color and symbol to each idx based on the
 /// TileType.
-pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+pub fn draw_map(&mut self, map: &[TileType], ctx: &mut Rltk) {
     let mut y = 0;
     let mut x = 0;
 
@@ -53,104 +160,6 @@ pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
         if x > 79 {
             x = 0;
             y += 1;
-        }
-    }
-}
-
-/// Makes a map with solid boundaries and 400 randomly placed walls. No guarentees that
-/// it won't look awful.
-pub fn new_map_test() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; 80*50];
-    
-    for x in 0..80 {
-        map[xy_idx(x, 0)] = TileType::Wall;
-        map[xy_idx(x, 49)] = TileType::Wall;
-    }
-
-    for y in 0..50 {
-        map[xy_idx(0, y)] = TileType::Wall;
-        map[xy_idx(79, y)] = TileType::Wall;
-    }
-
-    // Now we will randomly splat a bunch of walls. It won't be pretty, 
-    // but it's a decent illustration.
-    // First, obtain te thread-local RNG:
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    for _i in 0..400 {
-        let x = rng.roll_dice(1, 79);
-        let y = rng.roll_dice(1, 49);
-        let idx = xy_idx(x, y);
-
-        if idx != xy_idx(40, 25) {
-            map[idx] = TileType::Wall;
-        }
-    }
-
-    map
-}
-
-/// Makes a map with two rooms connected by corridors.
-/// Returns a list of rooms.
-pub fn new_map_rooms_and_corridors() -> (Vec<Rect>, Vec<TileType>) {
-    let mut map = vec![TileType::Wall; 80*50];
-    
-    let mut rooms: Vec<Rect> = Vec::new();
-    const MAX_ROOMS: i32 = 30;
-    const MIN_SIZE: i32 = 6;
-    const MAX_SIZE: i32 = 10;
-
-    let mut rng = RandomNumberGenerator::new();
-
-    for _ in 0..MAX_ROOMS {
-        let w = rng.range(MIN_SIZE, MAX_SIZE);
-        let h = rng.range(MIN_SIZE, MAX_SIZE);
-        let x = rng.roll_dice(1, 80 - w - 1) - 1;
-        let y = rng.roll_dice(1, 50 - h - 1) - 1;
-        let new_room = Rect::new(x, y, w, h);
-        let mut ok = true;
-
-        for other_room in rooms.iter() {
-            if new_room.intersect(other_room) {
-                ok = false;
-            }
-        }
-        if ok {
-            apply_room_to_map(&new_room, &mut map);
-
-            if !rooms.is_empty() {
-                let (new_x, new_y) = new_room.center();
-                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
-
-                if rng.range(0, 2) == 1 {
-                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
-                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
-                } else {
-                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
-                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
-                }
-            }
-            rooms.push(new_room);
-        }
-    }
-
-    (rooms, map)
-}  
-
-fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
-    for x in min(x1, x2) ..= max(x1, x2) {
-        let idx = xy_idx(x, y);
-        if idx > 0 && idx < 80*50 {
-            map[idx as usize] = TileType::Floor;
-        }
-    }
-}
-
-fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
-    for y in min(y1, y2) ..= max(y1, y2) {
-        let idx = xy_idx(x, y);
-        if idx > 0 && idx < 80*50 {
-            map[idx as usize] = TileType::Floor;
         }
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,18 +1,18 @@
 use rltk::{VirtualKeyCode, Rltk};
 use specs::prelude::*;
-use super::{Position, Player, TileType, xy_idx, State};
+use super::{Position, Player, TileType, State, Map};
 use std::cmp::{min, max};
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>(); // Feels odd to couple map to the player like this.
+    let map = ecs.fetch::<Map>(); // Feels odd to couple map to the player like this.
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
 
         // If we are not on a wall.
-        if map[destination_idx] != TileType::Wall {
+        if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(79, max(0, pos.y + delta_y));
         }
